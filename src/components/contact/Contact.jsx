182cdab874d24bcb3bb1490cb34b174d906ba280
Shrish Dhakal <shrishd242@gmail.com>
2024-01-20 13:07:12 +0545

    updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import styles from '../contact/Contact.module.css'
+"use client"
+
+import React, { useState } from 'react';
+import styles from '../contact/Contact.module.css';
+import contact from '../../app/api/Contact.js'
 
 const Contact = () => {
+
   return (
     <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
         <p className={styles.title}>Want to Contact me?</p>
-        <p className={styles.subtitle}>Now more easier access. Just send me your queries here and I am one click away</p>
+        <p className={styles.subtitle}>
+          Now more easier access. Just send me your queries here and I am one
+          click away
+        </p>
         <div className={styles.textarea}>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <button className={styles.button}>Send</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+          />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" className={styles.button}>
+            Send
+          </button>
         </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
